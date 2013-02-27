82278c626671be6eb7e2e9fd78f7ea592eb1a7f8
web32 <web32@5thDimensionGames.de.ve>
2013-02-27 14:41:05 +0100

    Changed license text

--- src/main/java/me/web32/MineCaster/Main.java
+++ src/main/java/me/web32/MineCaster/Main.java
@@ -1,6 +1,30 @@
-/* The file Main.java was created.
-* Copyright (c) 2013 Maximilian Söllner alias web32. All rights reserved.
-*/
+/*
+ * Copyright 2013 Maximilian Soellner. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright notice, this list of
+ *       conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *       of conditions and the following disclaimer in the documentation and/or other materials
+ *       provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are those of the
+ * authors and contributors and should not be interpreted as representing official policies,
+ * either expressed or implied, of anybody else.
+ */
 
 package me.web32.MineCaster;
 
@@ -177,7 +201,11 @@ public class Main extends JavaPlugin{
             broadcastingSchedueler = this.getServer().getScheduler().runTaskTimerAsynchronously(this, new Runnable() {
                 @Override  
                 public void run() {
-                    if(messages.size() < 1) return;
+                    if(messages.size() < 1) {
+                        return;
+                    } else if (getServer().getOnlinePlayers().length == 0) {
+                        return;
+                    }
                     if(random) {
                         int randomInt = Math.round((float) Math.random() * (messages.size()-1));
                         Broadcaster.broadcast(prefix.getMessage(), messages.get(randomInt).getMessage());
